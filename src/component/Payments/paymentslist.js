1972fd1a5df009d3a47732f4186cb628b749a5f4
chintantis <chintan@thinkinfoservices.com>
2023-04-18 16:22:37 +0530

    Build error solving

--- src/component/Payments/paymentslist.js
+++ src/component/Payments/paymentslist.js
@@ -293,7 +293,7 @@ const EnhancedTable = (props) => {
                       : categoryList
                     ).map((item, index) => {
                       return (
-                        <TableRow>
+                        <TableRow key={index}>
                           <TableCell>{item.category}</TableCell>
                           <TableCell className="content_end">
                             <Box style={{ display: "flex" }}>
